d4a2471c9ecbd7db1d56a713ca6e356ed82a2f93
cenkh <1250578320@qq.com>
2015-08-28 09:39:20 +0800

    iOS8 Photos.framework练手

---
+++ PhotosDemo/PhotosDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotosDemo
+//
+//  Created by LJ on 15/8/28.
+//  Copyright (c) 2015年 广东道一信息科技有限公司. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
